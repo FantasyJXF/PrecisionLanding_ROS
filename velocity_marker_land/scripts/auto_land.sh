70823ee321b8ac84e59900ac766eec01368d333b
Fantasy <fantasyjxf@qq.com>
2019-05-04 14:18:58 +0800

    to test it

--- velocity_marker_land/scripts/auto_land.sh
+++ velocity_marker_land/scripts/auto_land.sh
@@ -1,1 +1,11 @@
+#! /bin/bash
+
+# This script works, use `gnome-terminal -x xxx.sh` command
+
+source /opt/ros/kinetic/setup.bash
+source /home/odroid/catkin_ws/devel/setup.bash
+source /home/odroid/Apriltag_ROS/apriltags_ws/devel/setup.bash
+
 roslaunch velocity_marker_land auto_land.launch
+
+exit 0
